65c8ffc0aa85328724c5f8f9e9e39b25732eb51a
Pedro Belo <pedro@heroku.com>
2014-02-27 01:02:03 -0800

    grr

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,6 +1,8 @@
 require "clockwork"
 require "./lib/ombudsman"
 
+$stdout.sync = true
+
 module Clockwork
   every(5.minutes, "store-stats", thread: true) do
     Endpoint.dataset.use_cursor.each do |e|
