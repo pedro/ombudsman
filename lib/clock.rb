7034a66e62b78675b088b82ddda9813038894a22
Pedro Belo <pedro@heroku.com>
2014-02-27 01:00:10 -0800

    moar logs

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,8 +4,9 @@ require "./lib/ombudsman"
 module Clockwork
   every(5.minutes, "store-stats", thread: true) do
     Endpoint.dataset.use_cursor.each do |e|
-      e.stats = Stats.summary(e)
-      e.save
+      stats = Stats.summary(e)
+      puts "updating #{e.id}: #{stats.inspect}"
+      e.update(stats: stats)
     end
   end
 end
