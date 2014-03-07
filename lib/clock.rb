72f72c44f26e76be32232617bb9f42c6644c7689
Pedro Belo <pedro@heroku.com>
2014-03-06 18:49:58 -0800

    actually this has to match the Stats window

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,7 +4,7 @@ require "./lib/ombudsman"
 $stdout.sync = true
 
 module Clockwork
-  every(1.minutes, "store-stats", thread: true) do
+  every(5.minutes, "store-stats", thread: true) do
     Endpoint.dataset.use_cursor.each do |e|
       stats = Stats.summary(e)
       puts "updating #{e.id}: #{stats.inspect}"
